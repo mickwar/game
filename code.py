0964a5443751286e455fccf4b2b670d66de7975c
mickey <mickey.a.warner@gmail.com>
2019-04-29 22:54:35 -0600

    some enhanced moving

--- code.py
+++ code.py
@@ -92,6 +92,7 @@ def main():
                 # change the value to False, to exit the main loop
                 running = False
 
+        # Moving
         if not pygame.mouse.get_pressed()[0]:
             wait_flag = False
 
@@ -106,19 +107,44 @@ def main():
         if pygame.mouse.get_pressed()[0] and not wait_flag and focused:
             wait_flag = True
             pos = pixel_to_grid(*pygame.mouse.get_pos())
-            focused.move_exact(*pos, area)
-            focused = None
-            tmp_text = "nothing selected"
+            for s in sprites:
+                if pos == (s.x, s.y):
+                    focused = None
+                    tmp_text = "cannot move there"
+            if focused:
+                focused.move_exact(*pos, area)
+                focused = None
+                tmp_text = "nothing selected"
 
         if pygame.mouse.get_pressed()[2] and not wait_flag and focused:
             wait_flag = True
             focused = None
             tmp_text = "nothing selected"
 
-        area.screen.fill((16,16,16))
+
+
+        area.screen.fill((16, 16, 16))
         pygame.draw.rect(area.screen, (48, 48, 48),
             grid_to_pixel(area.xrange[0], area.yrange[0]) + \
             grid_to_pixel(area.xrange[1]+0.5, area.yrange[1]+0.5))
+
+        # Show the possible move options
+        if focused:
+            for dx in range(-focused.stat_move, focused.stat_move + 1):
+                for dy in range(-focused.stat_move, focused.stat_move + 1):
+                    tmp_pos = (focused.x - dx, focused.y - dy)
+                    # Make sure not moving over an existing unit
+                    for s in sprites:
+                        if tmp_pos == (s.x, s.y):
+                            break
+                    else:
+                        # Make sure within bounds and movement range
+                        if abs(dx) + abs(dy) <= focused.stat_move and \
+                            area.xrange[0] <= tmp_pos[0] and tmp_pos[0] <= area.xrange[1] and \
+                            area.yrange[0] <= tmp_pos[1] and tmp_pos[1] <= area.yrange[1]:
+                            pygame.draw.rect(area.screen, (0, 128, 0),
+                                grid_to_pixel(focused.x - dx, focused.y - dy) + \
+                                (GRID_TO_PIXEL-5, GRID_TO_PIXEL-5))
 
         # event handling, gets all event from the event queue
         #one.move(round(random.uniform(-1, 1)/1.99 * 1), round(random.uniform(-1, 1)/1.99 * 1), area)
@@ -126,15 +152,9 @@ def main():
             area.screen.blit(s.image, grid_to_pixel(s.x, s.y))
 
         #area.screen.blit(one.image, grid_to_pixel(one.x, one.y))
-        area.screen.blit(textsurface,(1,1))
+        area.screen.blit(textsurface,(8,4))
         textsurface = myfont.render(str(tmp_text), False, (128, 0, 0))
         pygame.display.flip()
-
-        for event in pygame.event.get():
-            # only do something if the event is of type QUIT
-            if event.type == pygame.QUIT:
-                # change the value to False, to exit the main loop
-                running = False
 
 
 if __name__ == "__main__":
