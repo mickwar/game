35ad6f8c015a73fe9fc1dffa08a913648532e6f3
mickey <mickey.a.warner@gmail.com>
2019-05-01 11:02:33 -0600

    attribute name changes

--- units.py
+++ units.py
@@ -8,31 +8,36 @@ class Unit(pygame.sprite.Sprite):
         self.x = 0
         self.y = 0
         self.name = name
-        # Inital stats
-        self.stat_hp       = 100
-        self.stat_mp       = 100
-        self.stat_bp       = 5
-        self.stat_strength = 10
-        self.stat_vitality = 10
-        self.stat_magic    = 10
-        self.stat_spirit   = 10
-        self.stat_move     = 3
-        self.stat_speed    = 8
+        # Base stats
+        self.base_hp_max   = 100
+        self.base_mp_max   = 100
+        self.base_bp_max   = 5
+        self.base_strength = 10
+        self.base_vitality = 10
+        self.base_magic    = 10
+        self.base_spirit   = 10
+        self.base_move     = 3
+        self.base_speed    = 8
         # Primary job stat adjustments
         self.job_stats(JobPrimary, 1)
         self.job_stats(JobSecondary, 2)
+        # Current stats
+        self.stat_hp = self.base_hp_max
+        self.stat_mp = self.base_mp_max
+        self.stat_bp = 1
+        self.stat_ct = 0
 
     def job_stats(self, Job = None, degree = 1):
         if Job is not None:
-            self.stat_hp       += int(Job.d_hp / degree)
-            self.stat_mp       += int(Job.d_mp / degree)
-            self.stat_bp       += int(Job.d_bp / degree)
-            self.stat_strength += int(Job.d_strength / degree)
-            self.stat_vitality += int(Job.d_vitality / degree)
-            self.stat_magic    += int(Job.d_magic / degree)
-            self.stat_spirit   += int(Job.d_spirit / degree)
-            self.stat_move     += int(Job.d_move / degree)
-            self.stat_speed    += int(Job.d_speed / degree)
+            self.base_hp_max   += int(Job.d_hp_max / degree)
+            self.base_mp_max   += int(Job.d_mp_max / degree)
+            self.base_bp_max   += int(Job.d_bp_max / degree)
+            self.base_strength += int(Job.d_strength / degree)
+            self.base_vitality += int(Job.d_vitality / degree)
+            self.base_magic    += int(Job.d_magic / degree)
+            self.base_spirit   += int(Job.d_spirit / degree)
+            self.base_move     += int(Job.d_move / degree)
+            self.base_speed    += int(Job.d_speed / degree)
 
     def move_delta(self, dx, dy, Map):
         self.x = min(max(Map.xrange[0], self.x + dx), Map.xrange[1])
@@ -42,7 +47,7 @@ class Unit(pygame.sprite.Sprite):
     # moves outside of the class, unless the get_paths method should be part
     # of this class?
     def move_exact(self, dx, dy, Map):
-        if abs(self.x - dx) + abs(self.y - dy) <= self.stat_move and \
+        if abs(self.x - dx) + abs(self.y - dy) <= self.base_move and \
             Map.xrange[0] <= dx and dx <= Map.xrange[1] and \
             Map.yrange[0] <= dy and dy <= Map.yrange[1]:
             self.x = dx
@@ -56,7 +61,7 @@ class Unit(pygame.sprite.Sprite):
             dx = p[0] - self.paths[0][0]
             dy = p[1] - self.paths[0][1]
             # Break out of loop if total distance exceeds move limit
-            if abs(dx) + abs(dy) >= self.stat_move:
+            if abs(dx) + abs(dy) >= self.base_move:
                 break
             for d in directions:
                 # Add current location p to directional movement d
@@ -85,23 +90,23 @@ class Unit(pygame.sprite.Sprite):
 
 
 class Type1():
-    d_hp = 10
-    d_mp = 10
-    d_bp = 10
+    d_hp_max = 10
+    d_mp_max = 10
+    d_bp_max = 10
+    d_strength = 10
+    d_vitality = 10
+    d_magic = 10
+    d_spirit = 10
+    d_move = 0
+    d_speed = 2
+
+class Type2():
+    d_hp_max = 10
+    d_mp_max = 10
+    d_bp_max = 10
     d_strength = 10
     d_vitality = 10
     d_magic = 10
     d_spirit = 10
     d_move = 1
-    d_speed = 1
-
-class Type2():
-    d_hp = 10
-    d_mp = 10
-    d_bp = 10
-    d_strength = 10
-    d_vitality = 10
-    d_magic = 10
-    d_spirit = 10
-    d_move = 3
     d_speed = 1
